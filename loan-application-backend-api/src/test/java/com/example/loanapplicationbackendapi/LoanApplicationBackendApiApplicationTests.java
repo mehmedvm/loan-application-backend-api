d8d5ca7a4cf491c2eff5d7e2c9bb9f242f9dbf14
MehmedVM <mehmedvm@gmail.com>
2022-05-25 17:47:23 -0400

    5-25 v1

---
+++ loan-application-backend-api/src/test/java/com/example/loanapplicationbackendapi/LoanApplicationBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanapplicationbackendapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApplicationBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
